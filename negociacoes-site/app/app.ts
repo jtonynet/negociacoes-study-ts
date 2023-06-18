522ee714f505d389a82f6b373e3fb698a9f08f8b
jtonyNet <jtony.net@gmail.com>
2023-06-18 01:14:34 -0300

    hotfix: strictNullChecks true e correcao no Dockerfile de negociacao-site

--- negociacoes-site/app/app.ts
+++ negociacoes-site/app/app.ts
@@ -3,11 +3,11 @@ import { NegociacaoView } from "./views/negociacoes-view.js";
 
 const controller = new NegociacaoController();
 const form = document.querySelector('.form');
-form.addEventListener('submit', event => {
-    event.preventDefault();
-    controller.adiciona();
-});
-
-//const negociacaoView = new NegociacaoView;
-//const template = negociacaoView.template()
-//console.log(template);
+if(form) {
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        controller.adiciona();
+    });
+} else {
+    throw Error('Não foi possível inicializar a aplicação. Verifique se o formulário existe.');
+}
--- negociacoes-site/dist/js/app.js
+++ negociacoes-site/dist/js/app.js
@@ -1,10 +1,12 @@
 import { NegociacaoController } from "./controllers/negociacao-controller.js";
 const controller = new NegociacaoController();
 const form = document.querySelector('.form');
-form.addEventListener('submit', event => {
-    event.preventDefault();
-    controller.adiciona();
-});
-//const negociacaoView = new NegociacaoView;
-//const template = negociacaoView.template()
-//console.log(template);
+if (form) {
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        controller.adiciona();
+    });
+}
+else {
+    throw Error('Não foi possível inicializar a aplicação. Verifique se o formulário existe.');
+}
