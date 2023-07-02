1d8c932600d37880c8228165002ed121799beebb
jtonyNet <jtony.net@gmail.com>
2023-07-02 14:33:33 -0300

    feat(decorator de propriedade): domInjector para manipular elementos de DOM)

---
+++ packages/negociacoes-site/src/decorators/dom-injector.ts
@@ -0,0 +1,17 @@
+export function domInjector(seletor: string) {
+ return function (target: any, propertyKey: string) {
+    // console.log(`Modificando prototype ${target.constructor.name}
+    // e adicionando getter para a propriedade ${propertyKey}`);
+    const getter = function() {
+        const elemento = document.querySelector(seletor);
+        //console.log(`buscando buscando elemento do DOM com seletor ${seletor} para injetar em ${propertyKey}`);
+        return elemento;
+    }
+
+    Object.defineProperty(
+        target,
+        propertyKey,
+        {get: getter}
+    )
+ }
+}
--- packages/negociacoes-site/src/views/view.ts
+++ packages/negociacoes-site/src/views/view.ts
@@ -15,8 +15,6 @@ export abstract class View<T> {
         
     }
 
-    @logarTempoDeExecucao(true)
-    @inspect()
     update(model: T): void {
         let template = this.template(model);
         this.elemento.innerHTML = template
