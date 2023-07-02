a8467a6741157217cfd86143f3fb56dc44847357
jtonyNet <jtony.net@gmail.com>
2023-07-02 14:58:38 -0300

    feat(decorator de propriedade): melhoria no domInjector, usando o escopo de clojure como cache

--- packages/negociacoes-site/src/decorators/dom-injector.ts
+++ packages/negociacoes-site/src/decorators/dom-injector.ts
@@ -1,10 +1,16 @@
 export function domInjector(seletor: string) {
  return function (target: any, propertyKey: string) {
-    // console.log(`Modificando prototype ${target.constructor.name}
-    // e adicionando getter para a propriedade ${propertyKey}`);
+    console.log(`Modificando prototype ${target.constructor.name}
+    e adicionando getter para a propriedade ${propertyKey}`);
+
+    let elemento: HTMLElement;
+
     const getter = function() {
-        const elemento = document.querySelector(seletor);
-        //console.log(`buscando buscando elemento do DOM com seletor ${seletor} para injetar em ${propertyKey}`);
+        if (!elemento) {
+            elemento = <HTMLElement>document.querySelector(seletor);
+            console.log(`buscando buscando elemento do DOM com seletor ${seletor} para injetar em ${propertyKey}`);
+        }
+
         return elemento;
     }
 
