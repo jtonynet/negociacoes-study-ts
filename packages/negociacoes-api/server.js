116daab6f9b6ca76451db8b411f3ffdb05644920
jtonyNet <jtony.net@gmail.com>
2023-07-03 00:02:36 -0300

    ultima aula de TS

--- packages/negociacoes-api/server.js
+++ packages/negociacoes-api/server.js
@@ -1,7 +1,7 @@
 var http = require('http')
     ,app = require('./config/express');
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 http.createServer(app).listen(port, function() {
     console.log('negociacoes-api escutando na porta: ' + port);
 });
--- packages/negociacoes-site/src/services/negociacoes-service.ts
+++ packages/negociacoes-site/src/services/negociacoes-service.ts
@@ -3,7 +3,7 @@ import { Negociacao } from "../models/negociacao.js";
 
 export class NegociacoesService {
     public obterNegociacoes(): Promise<Negociacao[]> {
-       return fetch('http://localhost:3001/dados')
+       return fetch('http://localhost:3003/dados')
             .then(res => res.json())
             .then((dados: NegociacaoDoDia[]) => {
                 return dados.map(dadoDeHoje => {
